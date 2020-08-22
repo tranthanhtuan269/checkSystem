8167562bfb9b578208657ff60703b98505f7402c
tuantt <tuantt@tohsoft.com>
2020-08-22 10:25:40 +0700

    css

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -38,16 +38,11 @@
                       <div class="col-sm-8">
                         <select class="custom-select mr-sm-2" id="timeInterval">
                           <option value="0" @if($config->value == 0) selected @endif>Chọn 1 khoảng thời gian...</option>
-                          <option value="1" @if($config->value == 1) selected @endif>Mỗi phút</option>
-                          <option value="2" @if($config->value == 2) selected @endif>Mỗi 5 phút</option>
-                          <option value="3" @if($config->value == 3) selected @endif>Mỗi 10 phút</option>
-                          <option value="4" @if($config->value == 4) selected @endif>Mỗi 15 phút</option>
-                          <option value="5" @if($config->value == 5) selected @endif>Mỗi 30 phút</option>
-                          <option value="6" @if($config->value == 6) selected @endif>Mỗi giờ</option>
-                          <option value="7" @if($config->value == 7) selected @endif>Mỗi 3 giờ</option>
-                          <option value="8" @if($config->value == 8) selected @endif>Mỗi 6 giờ</option>
-                          <option value="9" @if($config->value == 9) selected @endif>Mỗi 12 giờ</option>
-                          <option value="10" @if($config->value == 10) selected @endif>Mỗi ngày</option>
+                          <option value="1" @if($config->value == 1) selected @endif>Mỗi giờ</option>
+                          <option value="2" @if($config->value == 2) selected @endif>Mỗi 3 giờ</option>
+                          <option value="3" @if($config->value == 3) selected @endif>Mỗi 6 giờ</option>
+                          <option value="4" @if($config->value == 4) selected @endif>Mỗi 12 giờ</option>
+                          <option value="5" @if($config->value == 5) selected @endif>Mỗi ngày</option>
                         </select>
                       </div>
                     </div>
