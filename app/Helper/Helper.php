101706af2003d2f1ca43b543eb0ddf97f2633170
tuantt <tuantt@tohsoft.com>
2020-08-25 17:31:45 +0700

    css

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -7,12 +7,43 @@ class Helper
 {
     public static function http_response($url, $status = null, $wait = 3)
     {
-        $fp = @fsockopen($url, $port=80, $errno, $errstr, 2);
-        if (!$fp) {
+        $time = microtime(true);
+        $expire = $time + $wait;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $head = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+        
+        if(!$head)
+        {
             return 0;
-        } else{
-           return 1;
+        }else{
+            return 1;
         }
+        
+        if($status === null)
+        {
+            if($httpCode < 400)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        elseif($status == $httpCode)
+        {
+            return 1;
+        }
+        
+        return FALSE;
     }
 
 	public static function SendMail($title, $email, $content, $content_email=[]){
