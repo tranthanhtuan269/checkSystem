fab7f0f34153147f55f49b5965f524caccd93fa0
tuantt <tuantt@tohsoft.com>
2020-08-25 14:06:48 +0700

    css

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -18,7 +18,7 @@ class Helper
         $head = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        dd($httpCode);
+        dd("$httpCode:" . $httpCode);
         dd($status);
         dd(!$head);
 
