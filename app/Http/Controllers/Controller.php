fa4fbffd3312a0ada40a0d16ee52d6dd5cf39001
Ba TV <batv@tohsoft.com>
2021-01-04 17:32:47 +0700

    css

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -26,7 +26,5 @@ class Controller extends BaseController
             setcookie('cache_visited', "visited", time() + 86400 );// 1 day
             // dd(Cache::get('cache_visited'));
         }
-
-        dd(Cache::get('cache_visited'));
     }
 }
